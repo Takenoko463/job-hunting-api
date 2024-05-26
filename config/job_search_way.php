dd2f7f151eac31b9b36d988db9022e30d38701d3
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-05-27 08:23:15 +0900

    データベースにまとめるまでもない情報はconfigに記載

---
+++ config/job_search_way.php
@@ -0,0 +1,53 @@
+<?php
+return [
+  [
+    'id' => 0,
+    'name' => 'paiza',
+    'url' =>  'https://paiza.jp/en_try/'
+  ],
+  [
+    'id' => 1,
+    'name' => 'div',
+    'url' =>  'https://div.capid.jp/'
+  ],
+  [
+    'id' => 2,
+    'name' => 'Green',
+    'url' =>  'https://www.green-japan.com/'
+  ],
+  [
+    'id' => 3,
+    'name' => 'doda',
+    'url' =>  'https://doda.jp/'
+  ],
+  [
+    'id' => 4,
+    'name' => 'Wantedly',
+    'url' =>  'https://sg.wantedly.com/'
+  ],
+  [
+    'id' => 5,
+    'name' => 'エン転職',
+    'url' =>  'https://employment.en-japan.com/'
+  ],
+  [
+    'id' => 6,
+    'name' => 'type',
+    'url' =>  'https://type.jp/'
+  ],
+  [
+    'id' => 7,
+    'name' => 'マイナビ',
+    'url' =>  'https://tenshoku.mynavi.jp/'
+  ],
+  [
+    'id' => 8,
+    'name' => 'リクナビ',
+    'url' =>  'https://employment.en-japan.com/'
+  ],
+  [
+    'id' => 9,
+    'name' => 'その他',
+    'url' =>  null
+  ]
+];
