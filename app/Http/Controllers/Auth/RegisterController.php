824ce54b815063f28260e604fab1950994bb5486
Takenoko463 <83239026+Takenoko463@users.noreply.github.com>
2024-05-27 19:39:06 +0900

    login register機能の実装

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,54 @@
+<?php
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\HasApiTokens;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
+
+class RegisterController extends Controller
+{
+    use HasApiTokens;
+
+    /**
+     * Handle a registration request for the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(RegisterRequest $request)
+    {
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['access_token' => $token, 'token_type' => 'Bearer']);
+    }
+
+    /**
+     * Handle a login request to the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(LoginRequest $request)
+    {
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+        
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['access_token' => $token, 'token_type' => 'Bearer']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
-
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GreetingController;
+use App\Http\Controllers\Auth\RegisterController;
+
 
 Route::get('/greeting', 
 [GreetingController::class, 'index']);
@@ -14,3 +17,6 @@ Route::get('/user', function (Request $request) {
 use App\Http\Controllers\JobHuntingStatusController;
 Route::apiResource('user.job_hunting_statuses',JobHuntingStatusController::class)->only('index');
 Route::apiResource('job_hunting_statuses',JobHuntingStatusController::class)->except('index');
+
+Route::post('/register', [RegisterController::class, 'register']);
+Route::post('/login', [RegisterController::class, 'login']);
